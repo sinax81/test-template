6931b30bf3244affd278ba580c2e7e8a5ebb5b43
sinax81 <sinxweb@gmail.com>
2021-08-25 20:34:36 +0430

    test-new

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+    crossorigin="anonymous"
+  />
+  <link href="style/style-index.css" type="text/css" rel="stylesheet" />
+  <link href="style/style-all.css" type="text/css" rel="stylesheet" />
+  <!-------header-->
+  <?php
+  include("header.html")
+  ?>
+  <body>
+    <div class="box-slider">
+      <div
+        class="carousel slide carousel-fade"
+        data-interval="5000"
+        data-ride="carousel"
+        id="slider"
+      >
+        <ul class="carousel-indicators">
+          <li class="active" data-target="slider" data-slide-to="0"></li>
+          <li data-target="slider" data-slide-to="1"></li>
+          <li data-target="slider" data-slide-to="2"></li>
+          <li data-target="slider" data-slide-to="3"></li>
+        </ul>
+        <div class="box-control">
+          <a class="c-next" href="#slider" role="button" data-slide="next">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+          </a>
+          <a class="c-next" href="#slider" role="button" data-slide="prev">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+</svg>
+          </a>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item box-slider-item active">
+            <img src="img/01.jpg" alt="img" class="d-block" />
+            <div class="carousel-caption cc-h1">
+              <h1 class="c-h1">LEGO <br />&nbsp; HOUSE</h1>
+            </div>
+          </div>
+
+          <div class="carousel-item box-slider-item">
+            <img src="img/02.jpg" alt="img" class="d-block" />
+            <div class="carousel-caption">
+              <h1 class="c-h1 text-white">LEGO <br />&nbsp; HOUSE</h1>
+            </div>
+          </div>
+
+          <div class="carousel-item box-slider-item">
+            <div class="carousel-caption">
+              <h1 class="c-h1 text-white">LEGO <br />&nbsp; HOUSE</h1>
+            </div>
+            <img src="img/03.jpg" alt="img" class="d-block" />
+          </div>
+
+          <div class="carousel-item box-slider-item">
+            <div class="carousel-caption">
+              <h1 class="c-h1">LEGO <br />&nbsp; HOUSE</h1>
+            </div>
+            <img src="img/04.jpg" alt="img" class="d-block" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!---------------------------------------------------------------------slider-->
+    <div class="box-2">
+      <p>title</p>
+      <div class="text-box">
+        <div class="c2">
+          <p>
+            MISE <br />
+            VAV DER<br />
+            ROHE<br />
+            AWARD
+          </p>
+        </div>
+        <div>
+          <p>
+            The PCR testing, which is the NAAT testing or that tests the nucleic
+            acid of the virus itself, are the most sensitive ones. And those,
+            
+          </p>
+        </div>
+        <div>
+          <p>
+            The PCR testing, which is the NAAT testing or that tests the nucleic
+            acid of the virus itself, are the most sensitive ones. And those,
+            The PCR testing, which is the NAAT testing or that tests the nucleic
+            
+          </p>
+        </div>
+        <div>
+          <p>
+            The PCR testing, which is the nucleic the NAAT testing or that tests
+            the nucleic<br />
+
+            <a class="btn"> more</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <!---------------------------------------------------------------------bax2-->
+    <div class="box-3">
+      <div class="text-box-2">
+        <p>TITLE</p>
+        <h6>acid of the virus itselfacid of the virus itself</h6>
+        <img class="w-100 mb-2" src="img/02.jpg" alt="img" />
+        <p>
+        acid of the virus itself, are the most sensitive ones. And those, acid
+ of the virus itself, are the most sen the virus itse the virus
+ itsesitive ones. And those, The PCR testing, which is the NAAT testing
+ or that tests the nucleic acid of the virus i
+
+        </p>
+        <a class="btn-2"> more</a>
+      </div>
+      <div class="text-box-2">
+        <p>TITLE</p>
+        <h6>acid of the virus itselfacid of the virus itself</h6>
+        <p>
+          acid of the virus itself, are the most sensitive ones. And those, acid
+          of the virus itself, are the most sen the virus itse the virus
+          itsesitive ones. And those, The PCR testing, which is the NAAT testing
+          or that tests the nucleic
+        </p>
+        <p>
+          itsesitive ones. And those, The PCR testing, which is the NAAT testing
+          or that tests the nucleic itsesitive ones.itsesitive ones. And thoseitsesitive ones. And those, The PCR 
+        </p>
+        <a class="btn-2"> more</a>
+      </div>
+      <div class="text-img">
+        <p>TITLE</p>
+        <h1>QUARTERA<br />RENOVATION</h1>
+        <span> </span>
+        <img src="img/06.jpg" alt="img" />
+        <div class="text-box-dark">
+          <p class="text">
+            acid of the virus itself, are the most sensitive ones. And those,
+            acid of the virus itself, are the most sen the virus itse the virus
+            itsesitive ones. And those, The PCR testing, which is the NAAT
+            testing or that tests the nucleic
+          </p>
+          <a class="btn-3"> more</a>
+        </div>
+      </div>
+    </div>
+    <!---------------------------------------------------------------------bax3-->
+    <div class="box-4">
+      <div class="box-img">
+        <h1> <b>1</b>ST<BR> PRIZE</h1>
+        <img src="img/07.jpg" alt="img">
+      </div>
+      <div class="box-4-text">
+        <p>TITLE</p>
+        <h6>titel acid of the text test <br />titel text test</h6>
+          <p>
+          acid of the virus itself, are the most sensitive ones. And those,
+          acid of the virus itself, are the most sen th
+          acid of the virus itself, are the most sensitive ones. And those,
+          acid of the virus itself, are the most sen the virus itse the virus virus itse the virus
+          </p>
+          <p>
+          the virus itself, are the most sensitive ones. And those
+         </p>
+        <a class="btn-4"> more</a>
+      </div>
+    </div>
+    <!---------------------------------------------------------------------bax4-->
+    <h1 class="shape-1">INTO</h1>
+    <span class="shape-2"> </span>
+    <span class="shape-3"> </span>
+    <span class="shape-4"> </span>
+    <span class="shape-5">05mm</span>
+    <span class="shape-6">title</span>
+    <span class="shape-7">text</span>
+  </body>
+  <script
+    src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"
+  ></script>
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"
+  ></script>
+  <script
+    src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+    crossorigin="anonymous"
+  ></script>
+</html>
